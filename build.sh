fcf6f30560f9ca6618a5529f634229b2dca7a82b
Hasan Taşdemir <tasdemirh21@itu.edu.tr>
2025-04-30 23:40:13 +0300

    NumPy sürüm uyumsuzluğu giderildi, paketler Python 3.11 ile uyumlu hale getirildi

--- build.sh
+++ build.sh
@@ -2,13 +2,22 @@
 # exit on error
 set -o errexit
 
+# Python sürümünü belirt
+export PYTHON_VERSION=3.11.4
+
 # Gerekli dizinleri oluştur
 mkdir -p results
 mkdir -p report
 mkdir -p data
 
+# Pip önbelleğini temizle
+pip cache purge || true
+
+# Güncel pip'i kur
+pip install --upgrade pip
+
 # Python paketlerini kur
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
 
 # Statik dosyaları topla (gerekiyorsa)
 # python manage.py collectstatic --no-input 
